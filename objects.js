a5225f9a18456c7c7bfcbebb9dc52fb286abdb2e
ivanrj7j <ivanrj7j@gmail.com>
2024-10-05 15:03:53 +0530

    added scaling

--- objects.js
+++ objects.js
@@ -29,19 +29,26 @@ function initializeObjects(objectData){
     
     let objects = [];
     let maxDistance;
+    let maxSize;
 
     let distances = objectData.map((obj) => {
         return obj.position;
     });
 
+    let sizes = objectData.map((obj) => {
+        return obj.diameter;
+    });
+
     maxDistance = Math.max(...distances);
+    maxSize = Math.max(...sizes);
 
     objectData.forEach(planet => {
         
         let massValue = planet.mass / massDivisionFactor;
-        let sunDistance = planet.position / (maxDistance*1.05*2);
+        let sunDistance = Math.pow(planet.position / (maxDistance*1.28), 1/3) / 2;
         let y = sunDistance+0.5;
-        let diameter = planet.diameter/scaleDivisionFactor;
+        let diameter = (planet.diameter/maxSize) + 0.1;
+        diameter = Math.tanh(diameter) * 200;
 
         let position = new Vector(0.5,y);
         let velocity = Vector.zero();
