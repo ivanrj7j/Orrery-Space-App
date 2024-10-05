c6a4adfdcd7fa359c64717c73978b5504bdd51ed
sfndiku <sfndiku11@gmail.com>
2024-10-05 11:40:36 +0530

    pushing index and sketch

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
+const screenWidth = 1000;
+const screenHeight = 1000;
+const screenVector = new Vector(screenWidth,screenHeight);
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(screenWidth, screenHeight);
 }
 
+
+
 function draw() {
+
   background(30);
+
+  objects.forEach(body => {
+    const [r, g, b] = body.color;
+    const absolutePosition = body.mass.position.multiply(screenVector)
+    fill(r, g, b);
+    stroke(0, 0, 0);
+    circle(absolutePosition.x, absolutePosition.y, body.diameter)
+  });
+
 }
