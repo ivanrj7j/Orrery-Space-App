59100356baff55baf1b4d42a9150dfdebdb94c31
ivanrj7j <ivanrj7j@gmail.com>
2024-10-05 09:21:57 +0530

    added mass

---
+++ mass.js
@@ -0,0 +1,20 @@
+class Mass{
+    constructor(mass, position, velocity){
+        this.mass = mass;
+        this.position = position;
+        this.velocity = velocity;
+    }
+
+    updatePosition(){
+        this.position = this.position.add(this.velocity);
+    }
+
+    applyForce(force){
+        const accl = force.divideScalar(this.mass);
+        this.velocity = this.velocity.add(accl);
+    }
+
+    update(){
+        this.updatePosition();
+    }
+}
